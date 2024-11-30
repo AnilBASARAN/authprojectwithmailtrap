ac05d8c392c4bcebee0932df500f49dd15e3236e
AnilBASARAN <nlbsrn@gmail.com>
2024-11-30 05:32:00 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+const app = express();
+app.use(express.json()); // allows us to parse imcoming requests:req.body
+
+dotenv.config(); // At the top of your index.js or server.js
+const PORT = process.env.PORT || 3000;
+app.use("/api/auth",authRoutes)
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// password: cdEsrdLpN8TcCwnx
---
+++ backend/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import { signup,login,logout,verifyEmail } from "../controllers/auth.controller.js";
+
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+router.post("/verify-email",verifyEmail);
+export default router;
